1430aa71abed77d1b12112678b7f190edb985b82
biswanathsingh1991 <biswanathsingh1991@gmail.com>
2018-12-09 02:13:49 +0530

    backend added

---
+++ fontend/src/components/topranking_component.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+
+class Toprankingcomp extends Component{
+
+    render(){
+        return(
+            <div>
+            <table>
+                <tr>
+                    <tb class="count">biswanath</tb>
+                    <tb class="user-img"> <img alt="not found"/></tb>
+                    <tb class="wpm">wpm</tb>
+                    <tb class="ago">ii</tb>
+                </tr>
+            </table>
+            </div>
+        );
+    }
+}
+
+
+export default Toprankingcomp;
